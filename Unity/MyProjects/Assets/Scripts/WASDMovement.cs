8159489fe86d7f97c9f8193c641c3975cc112864
Patricia Kuipers <pkpatriciakuipers@gmail.com>
2021-03-04 12:01:43 +0100

    Unity Controls

--- Unity/MyProjects/Assets/Scripts/WASDMovement.cs
+++ Unity/MyProjects/Assets/Scripts/WASDMovement.cs
@@ -1,25 +1,89 @@
 using UnityEngine;
+using System.Collections;
 
 public class WASDMovement : MonoBehaviour
 {
     // This is where the variables are declared
-    public float speed = 6f; // de beginsnelheid is 6
-    public float rotationSpeed = 200f;
-    // Update is called once per frame
-    void Update()
+
+    private Rigidbody rigidbody;
+
+    public float baseSpeed = 10.0f;
+
+    public float speed;
+    public float sprintSpeed = 20.0f;
+    public float gravity = 10.0f;
+    public float maxVelocityChange = 10.0f;
+    public bool canJump = true;
+    public float jumpHeight = 2.0f;
+    private bool grounded = false;
+
+
+
+    void Awake()
     {
-        float translation = 0;
-        float rotation = 0;
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.freezeRotation = true;
+        rigidbody.useGravity = false;
 
-        translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        speed = baseSpeed;
+    }
 
-        transform.Translate(0, 0, translation);
-        transform.Rotate(0, rotation, 0);
-
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+    void FixedUpdate()
+    {
+        if (grounded)
         {
-            speed = 15f; // speed gaat van 6 naar 15 wanneer shift is ingedrukt
+            // Calculate how fast we should be moving
+            Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            targetVelocity = transform.TransformDirection(targetVelocity);
+            targetVelocity *= speed;
+
+            // Apply a force that attempts to reach our target velocity
+            Vector3 velocity = rigidbody.velocity;
+            Vector3 velocityChange = (targetVelocity - velocity);
+            velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+            velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+            velocityChange.y = 0;
+            rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
+
+            // Jump
+            if (canJump && Input.GetKey(KeyCode.Space))
+            {
+                rigidbody.velocity = new Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.z);
+            }
+
+            float rotation = 0;
+            float rotationSpeed = 200f;
+
+            rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+            transform.Rotate(0, rotation, 0);
+
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                speed = sprintSpeed;
+            }
+
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                speed = baseSpeed;
+            }
+
         }
+
+        // We apply gravity manually for more tuning control
+        rigidbody.AddForce(new Vector3(0, -gravity * rigidbody.mass, 0));
+
+        grounded = false;
+    }
+
+    void OnCollisionStay()
+    {
+        grounded = true;
+    }
+
+    float CalculateJumpVerticalSpeed()
+    {
+        // From the jump height and gravity we deduce the upwards speed 
+        // for the character to reach at the apex.
+        return Mathf.Sqrt(2 * jumpHeight * gravity);
     }
 }
