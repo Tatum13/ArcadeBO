4f589a4779f3756d4d4a37a1f032d19d4f255ee9
Tatum13 <tatumdv2001@gmail.com>
2021-04-13 16:49:12 +0200

    animation namen veranderd

---
+++ Unity/MyProjects/Assets/Scripts/Player/animation/PlayerAnimationsScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerAnimationsScript : MonoBehaviour
+{
+
+     Animator animator;
+     
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+       // Debug.Log(animator);
+    }
+
+    void Update()
+    {
+    
+
+    }
+
+ 
+}
