b9f5ccba7a4af10f179cd0a8a417c267e686c515
Tatum13 <tatumdv2001@gmail.com>
2021-04-14 03:02:52 +0200

    Bulldor met animaties

--- Unity/MyProjects/Assets/Scripts/egg/Egg_script.cs
+++ Unity/MyProjects/Assets/Scripts/egg/Egg_script.cs
@@ -6,6 +6,9 @@ public class Egg_script : MonoBehaviour
 {
     public static int eggsAmount;
     public GameObject eggsCounter;
+    private bool goUp = true;
+    private bool isUp = false;
+    public float speed = 0.004f;
 
     private void Start()
     {
@@ -17,5 +20,20 @@ public class Egg_script : MonoBehaviour
     {
         transform.Rotate(0, 90 * Time.deltaTime, 0); //ei laten draaien
         eggsCounter.GetComponent<TMPro.TextMeshProUGUI>().text = "Eggs: " + eggsAmount; //dit laat de counter omhoog gaan
+
+        if (goUp)
+        {
+            transform.Translate(Vector3.up * speed * 4f);
+            isUp = true;
+            goUp = false;
+        }
+        
+        if (isUp)
+        {
+            transform.Translate(Vector3.down * speed * 4f);
+            goUp = true;
+            isUp = false;
+        }
+
     }
 }
